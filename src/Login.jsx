169375bb9e1d5e50b45a15a264ab34fd05753be4
Ruth Solomon <ru3t4h5@gmail.com>
2025-11-24 06:44:06 +0300

    done the registration with tailwind

--- src/Login.jsx
+++ src/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
       <div className="w-full max-w-sm bg-white shadow-md rounded-lg p-8">
         <h1 className="text-2xl font-bold text-center text-gray-800 mb-2">
-          Eternal Family
+         Sign Up
         </h1>
         <p className="text-center text-gray-600 mb-6">Welcome</p>
 
